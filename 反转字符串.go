da7672140c66ff495c195fcfdfca6fbcd010c4e2
AngleMAXIN <1678190746@qq.com>
2018-09-14 17:11:58 +0800

    all

---
+++ 反转字符串.go
@@ -0,0 +1,25 @@
+
+
+/*
+编写一个函数，其作用是将输入的字符串反转过来。
+
+示例 1:
+
+输入: "hello"
+输出: "olleh"
+示例 2:
+
+输入: "A man, a plan, a canal: Panama"
+输出: "amanaP :lanac a ,nalp a ,nam A"
+*/
+
+
+
+func reverseString(s string) string {
+    strl := make([]byte,0)
+    l := len(s) - 1
+    for i := l; i >= 0; i-- {
+        strl = append(strl, s[i])
+    }
+    return string(strl)
+}
